dfc06a65f39800fb453a5e089a378d65232beff2
Vishy Vatsa <83795860+vishyfortheworld@users.noreply.github.com>
2024-02-16 16:40:50 +0530

    Update seek.js

--- seek.js
+++ seek.js
@@ -1,5 +1,5 @@
-var speed = 15;
-var distance = 10;
+var speed = 25;
+var distance = 65;
 $('#speed button')[0].value = 15;
 $('#speed button')[1].value = 25;
 $('#speed button')[2].value = 40;
@@ -15,20 +15,44 @@ var local_cost = $('#local-cost')[0];
 
 var speedchange = function (x) {
     speed = parseInt(x.target.value);
-    console.log(x.target.value);
-    console.log(x.target);
-    console.log(speed);
-    console.log(distance);
-    gridx_cost.innerHTML = Math.floor((30 * speed * distance * 35) / 1000);
-    local_cost.innerHTML = Math.floor(((30 * speed * distance * 35) / 1000) * 1.3);
+    var max = 6624;
+    var gridbar = Math.floor((30 * speed * distance * 35) / 1000);
+    var IECbar = Math.floor(gridbar*1.3);
+
+    gridx_cost.innerHTML = gridbar;
+    local_cost.innerHTML = IECbar;
+
+    $("#gridbar").height($("#gridbar").parent().height()*(gridbar/max));
+    $("#IECbar").height($("#gridbar").parent().height()*(IECbar/max));
 }
 
 var seekchange = function (x) {
     distance = parseInt(x.target.value);
-    gridx_cost.innerHTML = Math.floor((30 * speed * distance * 35) / 1000);
-    local_cost.innerHTML = Math.floor(((30 * speed * distance * 35) / 1000) * 1.3);
+
+    var max = 6624;
+    var gridbar = Math.floor((30 * speed * distance * 35) / 1000);
+    var IECbar = Math.floor(gridbar*1.3);
+
+    gridx_cost.innerHTML = gridbar;
+    local_cost.innerHTML = IECbar;
+
+    $("#gridbar").height($("#gridbar").parent().height()*(gridbar/max));
+    $("#IECbar").height($("#gridbar").parent().height()*(IECbar/max));
+
 }
 
+var gridxbarfrac = 
+$(document).ready(function() {
+    var max = 6624;
+    var gridbar = Math.floor((30 * speed * distance * 35) / 1000);
+    var IECbar = Math.floor(gridbar*1.3);
+
+    gridx_cost.innerHTML = gridbar;
+    local_cost.innerHTML = IECbar;
+
+    $("#gridbar").height($("#gridbar").parent().height()*(gridbar/max));
+    $("#IECbar").height($("#gridbar").parent().height()*(IECbar/max));
+  });
 
 
 $('#seek')[0].onchange = seekchange;
